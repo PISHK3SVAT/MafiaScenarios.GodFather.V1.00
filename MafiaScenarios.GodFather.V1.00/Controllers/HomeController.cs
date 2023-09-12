f7fe6e987e5a952f61e3be15a9554eb289109976
PISHK3SVAT <mjek106206@gmail.com>
2023-09-13 02:26:27 +0330

    Add project files.

---
+++ MafiaScenarios.GodFather.V1.00/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+using MafiaScenarios.GodFather.V1._00.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MafiaScenarios.GodFather.V1._00.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MafiaScenarios.GodFather.V1.00/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MafiaScenarios.GodFather.V1._00.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
