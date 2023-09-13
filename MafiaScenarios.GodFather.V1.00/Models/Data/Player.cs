b0bb1e660e6edc87ae21074dceff07b0c09b3dc1
PISHK3SVAT <mjek106206@gmail.com>
2023-09-13 11:40:50 +0330

    Player Model added

---
+++ MafiaScenarios.GodFather.V1.00/Models/Data/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace MafiaScenarios.GodFather.V1._00.Models.Data
+{
+    public class Player
+    {
+        public string? Name { get; set; }
+        public Card? Card { get; set; }
+    }
+}
